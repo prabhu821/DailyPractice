8b88e939b1a6e68a0dcf0bbe822ac8bcdd2ab7b6
rishiraj007 <rishiraj@mounttalent.com>
2022-12-27 23:38:02 +0530

    [Prabhu]: Method Overloading

---
+++ DailyPracticeProblem/MethodOverloading/Method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyPracticeProblem.MethodOverloading
+{
+    internal class Method
+    {
+        public int Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            return sum;
+        }
+        public double Add(double a,
+                          double b, double c)
+        {
+            double sum = a + b + c;
+            return sum;
+        }
+    }
+}
--- DailyPracticeProblem/Program.cs
+++ DailyPracticeProblem/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace DailyPracticeProblem
+﻿using DailyPracticeProblem.MethodOverloading;
+
+namespace DailyPracticeProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Method method = new Method();
             Console.WriteLine("Daily Practice Problem");
+           
+
+            int sum = method.Add(2, 4, 6);
+            Console.WriteLine("sum of the three "
+                              + "integer value : " + sum);
+            double doubleSum = method.Add(1.0, 2.4, 3.0);
+            Console.WriteLine("sum of the three "
+                              + "double value : " + doubleSum);
         }
     }
 }
