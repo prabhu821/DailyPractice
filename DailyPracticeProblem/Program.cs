3732aad1d1a31f164a2000d1c56299ad80cd7564
rishiraj007 <rishiraj@mounttalent.com>
2022-12-27 23:49:49 +0530

    [Prabhu]: Collection Demo

--- DailyPracticeProblem/Program.cs
+++ DailyPracticeProblem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DailyPracticeProblem.MethodOverloading;
+using DailyPracticeProblem.CollectionDemo;
 
 namespace DailyPracticeProblem
 {
@@ -6,16 +7,23 @@ namespace DailyPracticeProblem
     {
         static void Main(string[] args)
         {
-            Method method = new Method();
-            Console.WriteLine("Daily Practice Problem");
-           
+            //Method method = new Method();
+            //Console.WriteLine("Daily Practice Problem");
 
-            int sum = method.Add(2, 4, 6);
-            Console.WriteLine("sum of the three "
-                              + "integer value : " + sum);
-            double doubleSum = method.Add(1.0, 2.4, 3.0);
-            Console.WriteLine("sum of the three "
-                              + "double value : " + doubleSum);
+
+            //int sum = method.Add(2, 4, 6);
+            //Console.WriteLine("sum of the three "
+            //                  + "integer value : " + sum);
+            //double doubleSum = method.Add(1.0, 2.4, 3.0);
+            //Console.WriteLine("sum of the three "
+            //                  + "double value : " + doubleSum);
+
+            Collection collection = new Collection();
+            Collection.List();
+            Collection.Queue();
+            Collection.Dictinory();
+            Collection.Stack();
+            Collection.Set();
         }
     }
 }
