e7acf3fdf65302071db02be4627120d60c2b53b0
rishiraj007 <rishiraj@mounttalent.com>
2022-12-27 23:31:51 +0530

    [Prabhu]: Daily Practice Problem

---
+++ DailyPracticeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Daily Practice Problem");
+        }
+    }
+}
